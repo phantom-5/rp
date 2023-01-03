5e97613f77d7869900898c703592f627d91c7903
Rudra Madhav Biswal <35427300+phantom-5@users.noreply.github.com>
2023-01-03 07:07:44 +0000

    useReducer complete

--- src/components/Hooks/UseReducer.tsx
+++ src/components/Hooks/UseReducer.tsx
@@ -7,6 +7,9 @@ type ACTION = {
 
 function counterReducer(state:number,action:ACTION){
     let value = action.payload
+    
+    if(!value){return state}
+
     switch(action.type){
         case 'ADD':
             return state+value
@@ -30,9 +33,9 @@ const UseReducer = () => {
     return (
         <div className="demo-container">
             <div className="demo-box" style={{display:'flex',flexDirection:'column',justifyContent:'center',alignItems:'center'}}>
-                <input type="number" className="form-control" ref={inputRef} />
+                <input type="number" className="form-control" ref={inputRef} placeholder={'Enter Value'} />
                 <button className="btn btn-sm btn-dark" onClick={()=>{dispatchCounterAction({type:'ADD',payload:parseInt(inputRef?.current?.value?inputRef?.current?.value:'0')})}}>Add</button>
-                <button className="btn-sm btn btn-dark"  onClick={()=>{dispatchCounterAction({type:'SUBTRACT',payload:parseInt(inputRef?.current?.value?inputRef?.current?.value:'0')})}}>Subtract</button>
+                <button className="btn-sm btn btn-dark mb-1"  onClick={()=>{dispatchCounterAction({type:'SUBTRACT',payload:parseInt(inputRef?.current?.value?inputRef?.current?.value:'0')})}}>Subtract</button>
                 <div className="lead">{counterState}</div>
             </div>
             <div className="demo-box">
@@ -41,6 +44,8 @@ const UseReducer = () => {
                     <li>useState and useReducer are interchangeable and they differ only in how we write code</li>
                     <li>useReducer forced to be more structured and scales well when state updates become complex or multiple state update logics are there</li>
                     <li>First one write the reducer function, then the initilizer funtion, then the hook, then use the state and dispath returned from hook</li>
+                    <li>Use it if the setState of useState not being used in more than 2 places else use useReducer</li>
+                    <li>Check Evernote for further details</li>
                 </ul>
             </div>
         </div>
