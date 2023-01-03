2da2bba0d48866b5bda083196ea9b8e6e7b0b6a9
Rudra Madhav Biswal <35427300+phantom-5@users.noreply.github.com>
2023-01-03 04:12:22 +0000

    first commit from codespace

--- src/components/Hooks/UseEffect/UseEffect.tsx
+++ src/components/Hooks/UseEffect/UseEffect.tsx
@@ -33,7 +33,7 @@ const UseEffect = () => {
         let flag = true
         if (name) {
             // if(flag) //this is wrong
-            fetchData(name).then(bio => flag && setBio(bio)) //flag is used inside then not outside so that if cleanup of next state update runs and current fetchData is pending then by the time it solves the instance of 'let flag' its pointing to will be false as a result of which Bio wont be set from the old state update.
+            fetchData(name).then(bio => flag && setBio(bio)) //flag is used inside then not outside so that if cleanup of next state update runs and current fetchData is pending then by the time it resolves the instance of 'let flag' its pointing to will be false as a result of which Bio wont be set from the old state update.
 
         }
         if (cleanup) {
@@ -55,8 +55,16 @@ const UseEffect = () => {
                 <button className="btn btn-sm btn-dark" style={{ marginRight: '4px' }} onClick={() => { setName('Alice') }}>Alice</button>
                 <button className="btn btn-sm btn-dark" style={{ marginRight: '4px' }} onClick={() => { setName('Bob') }}>Bob</button>
                 <button className="btn btn-sm btn-dark" onClick={() => { setName('Taylor') }}>Taylor</button>
-                {bio && <div className="lead">{bio}</div>}<br />
-                <button className="btn btn-sm btn-dark mt-2" onClick={() => { setName(''); setBio(''); setCleanup(!cleanup); }}>{cleanup ? 'Remove cleanup' : 'Add cleanup'}</button>
+                {bio && <div style={{padding:'4px',border:'1px solid cyan'}}>{bio}</div>}<br />
+                <button className="btn btn-sm btn-dark mt-1" onClick={() => { setName(''); setBio(''); setCleanup(!cleanup); }}>{cleanup ? 'Remove cleanup' : 'Add cleanup'}</button>
+                <br/>
+                <code>Click on Bob and then Taylor quickly in sequence.</code><br/><br/>
+                <span>Without Cleanup</span><br/>
+                <code>When you click on Bob a fetch request is made and it takes 6s to respond and when you click on Taylor next a fetch request is made which take 3s to respond. Taylor shows up first, but then after 3s Bob's bio replaces Taylor even though the latest clicked button is Taylor.</code><br/>
+                <span>With Cleanup</span><br/>
+                <code>This race condition is handled by nullifying the old request by either using Cleanup function with AbortController or a flag. Cleanup of next state update runs and if current fetchData is pending then by the time it resolves the instance of 'let flag' the old fetch logic is pointing to will become false as the cleanup triggered by current state update will run on lexical environment of previous state update.
+                </code>
+
             </div>
         </div>
     )
