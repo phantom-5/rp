0ec786630f47ce30e3f74c07158a0ff689a6faa6
Rudra Madhav Biswal <35427300+phantom-5@users.noreply.github.com>
2023-01-03 04:28:44 +0000

    useEffect page complete

--- src/components/Hooks/UseEffect/UseEffect.tsx
+++ src/components/Hooks/UseEffect/UseEffect.tsx
@@ -43,6 +43,21 @@ const UseEffect = () => {
         }
     }, [name, cleanup])
 
+    //reactive dependency
+    let example = 10
+    let exampleFn = () => {}
+
+    useEffect(()=>{
+        console.log('Reactive Dependency - 1', example)
+    },[example])
+
+    useEffect(()=>{
+        console.log('Reactive Dependency - 2(fn)',exampleFn)
+    },[exampleFn])
+
+    
+
+
     return (
         <div className="demo-container">
             <div className="demo-box text-center">
@@ -59,12 +74,19 @@ const UseEffect = () => {
                 <button className="btn btn-sm btn-dark mt-1" onClick={() => { setName(''); setBio(''); setCleanup(!cleanup); }}>{cleanup ? 'Remove cleanup' : 'Add cleanup'}</button>
                 <br/>
                 <code>Click on Bob and then Taylor quickly in sequence.</code><br/><br/>
+            </div>
+            <div className="demo-b">
                 <span>Without Cleanup</span><br/>
                 <code>When you click on Bob a fetch request is made and it takes 6s to respond and when you click on Taylor next a fetch request is made which take 3s to respond. Taylor shows up first, but then after 3s Bob's bio replaces Taylor even though the latest clicked button is Taylor.</code><br/>
                 <span>With Cleanup</span><br/>
                 <code>This race condition is handled by nullifying the old request by either using Cleanup function with AbortController or a flag. Cleanup of next state update runs and if current fetchData is pending then by the time it resolves the instance of 'let flag' the old fetch logic is pointing to will become false as the cleanup triggered by current state update will run on lexical environment of previous state update.
                 </code>
-
+            </div>
+            <div className="demo-box">
+                <div className="lead">Reactive Dependencies</div>
+                <code>useEffect dependency array not only work with props and state update but also will work with any other reactive value, for example it can be a simple let/var/const/function etc. but here the catch is if we use these on every rerender their reference might change as per Object.is comparision causing the effect to be triggered. </code><br/>
+                <br/>
+                <code>Check console 'Reactive Dependency' and try to press any of the button from other example to trigger rerender.</code>
             </div>
         </div>
     )
